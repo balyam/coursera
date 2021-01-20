d022ff3713defe928bdaa206b0f24d9fc7d99820
balyam <balyam@mail.ru>
2021-01-20 09:56:47 +0600

    complete week 2

--- to_json.py
+++ to_json.py
@@ -1,7 +1,10 @@
 import json
+from functools import wraps
 
 
 def to_json(func):
-    def wrapped():
-        pass
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        result = json.dumps(func(*args, **kwargs))
+        return result
     return wrapped
