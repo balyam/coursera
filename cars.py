25beaefde22177e1eba4d3a7127bac6a211898fa
balyam <bolatumarov@gmail.com>
2021-02-01 07:05:18 +0600

    start get car list function

--- cars.py
+++ cars.py
@@ -27,7 +27,7 @@ class Truck(CarBase):
     def __init__(self, car_type, photo_file_name, brand, carrying, body_whl):
         CarBase.__init__(self, car_type, photo_file_name, brand, carrying)
         try:
-            whl = self.body_whl.split("x")
+            whl = body_whl.split("x")
             self.body_length = float(whl[0])
             self.body_width = float(whl[1])
             self.body_height = float(whl[2])
@@ -35,10 +35,10 @@ class Truck(CarBase):
             self.body_length = 0
             self.body_width = 0
             self.body_height = 0
+            print(e)
 
     def get_body_volume(self):
         return self.body_height * self.body_length * self.body_width
-
 
 
 class SpecMachine(CarBase):
@@ -51,8 +51,11 @@ class SpecMachine(CarBase):
 def get_car_list(csv_filename):
     car_list = []
     with open(csv_filename) as csv_fd:
-        reader = csv.reader(csv_fd, delimiter=';')
+        reader = csv.DictReader(csv_fd, delimiter=';')
         next(reader)  # пропускаем заголовок
         for row in reader:
             print(row)
     return car_list
+
+if __name__ == '__main__':
+    pass
