5c3d27fc453878bc7edb04a16ec9b3405fc22dbd
balyam <balyam@mail.ru>
2021-02-15 17:35:16 +0600

    finish solution for week 4

--- week4_file.py
+++ week4_file.py
@@ -9,33 +9,33 @@ class File:
     TEMP_PATH = tempfile.gettempdir()
 
     def __init__(self, path_to_file):
+        self.path = path_to_file
         if not os.path.isfile(path_to_file):
-            with open(path_to_file, 'w') as f:
+            with open(self.path, 'w') as f:
                 f.write("")
-        # else:
-        #     with open(path_to_file, 'r') as f:
-        #         f.read()
+        else:
+            with open(path_to_file, 'r') as f:
+                f.read()
 
     def __add__(self, file):
         file_name = ''.join(choice(digits) for i in range(5))
         new_file = type(self)(os.path.join(self.TEMP_PATH, file_name))
-        new_file.write(self.read)
-        new_file.write(file)
+        new_file.write(self.read() + file.read())
         return new_file
 
     def __iter__(self):
-        return self
+        return open(self.path, 'r')
 
     def __next__(self):
-        pass
+        return next(self)
 
     def __str__(self):
-        return f"{os.path.abspath(self)}"
+        return f"{os.path.abspath(self.path)}"
 
     def read(self):
-        with open(self, 'r') as f:
+        with open(self.path, 'r') as f:
             return f.read()
 
     def write(self, value_string):
-        with open(self, 'a') as f:
+        with open(self.path, 'w') as f:
             f.write(value_string)
