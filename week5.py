2f62a0ef998c55a243ce51cf88eddf5d168e3bdf
balyam <bolatumarov@gmail.com>
2021-02-22 23:24:48 +0600

    bad descriptor error

--- week5.py
+++ week5.py
@@ -1,24 +1,30 @@
 import os
 import socket
-from time import time
-
-
-
+import time
 
 
 class Client:
     def __init__(self, conn, port, timeout=None):
-        self.conn = conn
-        self.port = port
-        self.timeout = timeout
-        socket.create_connection((self.conn, self.port), self.timeout)
+        with socket.create_connection((conn, port), timeout) as sock:
+            self.sock = sock
+            self.sock.settimeout(0)
 
-    def get(self, metric_value):
-        raise ClientException
+    def get(self, metric):
+        msg = metric.encode()
+        self.sock.sendall(msg)
+        recieved_data = []
+        while True:
+            response = self.sock.recv(1024)
+            if not response:
+                print(recieved_data)
+                break
+            else:
+                recieved_data.append(response.decode('utf-8'))
 
     def put(self, metric, metric_value, timestamp=int(time.time())):
         raise ClientException
 
 
-class ClientException(Exception):
-    pass
+class ClientException(socket.error):
+    def __str__(self):
+        return f"<ClientException> message"
